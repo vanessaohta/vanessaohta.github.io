f0e5de0bed2177ba322e6a8b3edfae3b4adb21e8
vanessaohta <vanessaohta@Vanessas-MacBook-Air-3.local>
2017-03-29 21:11:45 -0700

    updating in lab time

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 // Make it rain!
+
+// When the user scrolls down the window
+	// If the distance scrolled is greater than or equal to 640px
+		// Make nav fixed
+	// else 
+		// Make nav static
+var element_position = $('nav').offset().top;
+console.log(element_position);
+
+$(window).on('scroll', function(event) {
+	event.preventDefault;
+    var y_scroll_pos = window.pageYOffset;
+    var scroll_pos_test = element_position;
+
+    if(y_scroll_pos > scroll_pos_test) {
+        $('nav').addClass('sticky');
+    } else {
+    	$('nav').removeClass('sticky');
+    }
+});
+
+//notes and resources for slider: 
+	//http://www.catchmyfame.com/2009/06/25/jquery-beforeafter-plugin/
